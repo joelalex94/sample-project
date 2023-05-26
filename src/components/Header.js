a2a3a4c5f87f6d1aff73f1f3b35ee2baac1a21d2
Justin Joseph <justin.j@vofox.com>
2023-05-26 20:14:19 +0530

    menu section

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,10 @@ import React, {useState} from 'react';
 import {Link, useLocation} from "react-router-dom";
 
 import logo from './company.png';
+import './Header.css';
 
 const Header = () => {
-    const [activeTab, setActivTab] = useState("Home");
+    const [activeTab, setActiveTab] = useState("Home");
     const [isSideNavOpen, setIsSideNavOpen] = useState(false);
 
     const toggleSideNav = () => {
@@ -14,36 +15,36 @@ const Header = () => {
     return ( 
         <>
         
-        <nav className="navbar navbar-light bg-light">
-            <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
                 <div className="navbar-brand">
                     <img src={logo} alt="logo" />
                 </div>
-                <button className="btn btn-secondary d-flex" onClick={toggleSideNav}> 
+                <button className="btn btn-secondary d-flex"  onClick={toggleSideNav}> 
                     <i className="bi bi-list"></i>
                 </button>
                 {isSideNavOpen && (
-                    <div className="side-nav">
+                    <div className=" side-nav">
                         {/* Side Nav Content Goes Here */}
-                        <ul className="nav">
+                        <ul className="navbar-nav">
                             <Link to='/'>
-                                <li className="nav-item active">
-                                    <label className="nav-link active">Home</label>
+                                <li className="nav-item ">
+                                    <label className={"nav-link " + (activeTab === "Home" ? 'active' : '')} onClick= { () => setActiveTab("Home")} >Home</label>
                                 </li>
                             </Link>
-                            <Link to='/employee'>
-                            <li className="nav-item active">
-                                <label className="nav-link ">Employee</label>
+                            <Link to='/employee'> 
+                            <li className="nav-item">
+                                <label  className={"nav-link " + (activeTab === "Employee" ? 'active' : '')} onClick= { () => setActiveTab("Employee")}>Employee</label>
                             </li>
                             </Link>
                             <Link to='/extra-hours'>
-                            <li className="nav-item active">
-                                <label className="nav-link ">Extra Hours</label>
+                            <li className="nav-item">
+                                <label   className={"nav-link " + (activeTab === "Extrahour" ? 'active' : '')} onClick= { () => setActiveTab("Extrahour")}>Extra Hours</label>
                             </li>
                             </Link>
                             <Link to='/order'>
-                            <li className="nav-item active">
-                                <label className="nav-link">Order</label>
+                            <li className="nav-item">
+                                <label   className={"nav-link " + (activeTab === "Order" ? 'active' : '')} onClick= { () => setActiveTab("Order")}>Order</label>
                             </li>
                             </Link>
                             
--- src/pages/Order/index.js
+++ src/pages/Order/index.js
@@ -1,7 +1,14 @@
 const Order = () => {
     return ( 
-        <div>
-            <h2>Order</h2>
+        <div className="card">
+            <div className="card-header">
+                Orders
+            </div>
+            <div className="card-body d-md-flex justify-content-md-end">
+                {/* <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
+                <a href="#" className="btn btn-primary ">Add Orders</a>
+            </div>
         </div>
      );
 }
