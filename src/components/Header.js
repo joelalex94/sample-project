43e701d9c6e02efd364d07c605adbe190c93f02f
Justin Joseph <justin.j@vofox.com>
2023-05-26 15:31:25 +0530

    First changes

---
+++ src/components/Header.js
@@ -0,0 +1,3 @@
+import React, {useEffect, useState} from 'react';
+
+import {Link, useLocation} from "react-router-dom";
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1SC64FQ-pJ0TepJhJDJqr24vyGHqj01U",
+  authDomain: "sample-project-3d044.firebaseapp.com",
+  projectId: "sample-project-3d044",
+  storageBucket: "sample-project-3d044.appspot.com",
+  messagingSenderId: "713160470419",
+  appId: "1:713160470419:web:a8f710494d95ad94444266"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+export default firedb.database().ref();
