574d65f1aab1a7516bef816fdc0bb6e1975c0cd7
Justin Joseph <justin.j@vofox.com>
2023-05-29 00:09:31 +0530

    Table commit

--- src/pages/Order/index.js
+++ src/pages/Order/index.js
@@ -2,21 +2,59 @@ import "./index.css";
 import {Link} from "react-router-dom";
 const Order = () => {
     return ( 
-        <div className="container">
-            <div className="card">
-                <div className="card-header">
-                    Orders
-                </div>
+        <>
+            <div className="container">
+                <div className="card">
+                    <div className="card-header">
+                        Orders
+                    </div>
 
-                <div className="card-body d-md-flex justify-content-md-end">
-                    {/* <h5 className="card-title">Special title treatment</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
-                    <Link to='/order/addorder'> 
-                        <label className="btn btn-primary ">Add Orders</label>
-                    </Link>
+                    <div className="card-body d-md-flex justify-content-md-end">
+                        {/* <h5 className="card-title">Special title treatment</h5>
+                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
+                        <Link to='/order/addorder'> 
+                            <label className="btn btn-primary ">Add Orders</label>
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className="container">
+                <div className="card">
+                    <div className="card-body d-md-flex justify-content-md-end">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">3</th>
+                            <td >Larry the Bird</td>
+                            <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </>
      );
 }
  
